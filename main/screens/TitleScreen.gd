4349614713ccd11c981d677eecd8d73b9c613eaa
ash4rk <cool.shark.98@mail.ru>
2023-06-09 01:02:24 +0300

    FEAT: Add shop menu button

--- main/screens/TitleScreen.gd
+++ main/screens/TitleScreen.gd
@@ -18,3 +18,20 @@ func _on_CreditsButton_pressed() -> void:
 
 func _on_coins_changed(new_value) -> void:
 	$Coins/HBoxContainer/CoinsValueLabel.text = str(new_value)
+
+func _on_Shop_pressed():
+	var from: Vector2 
+	var to: Vector2
+	
+	if not $HBoxContainer/VBoxContainer/Shop.pressed:
+		from = Vector2(0.0, 0.0)
+		to = Vector2(200.0, 0.0)
+	else:
+		from = Vector2(200.0, 0.0)
+		to = Vector2(0.0, 0.0)
+	
+	$Tween.interpolate_property($ShopWindow, "rect_position", from, 
+	to, 2.0, 
+	Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	$Tween.start()
+	
