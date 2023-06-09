eab39667d0ea7cd5b82c09aa92039c83fa4ea674
ash4rk <cool.shark.98@mail.ru>
2023-06-10 00:52:21 +0300

    FEAT: Add gain coins animation

--- main/screens/TitleScreen.gd
+++ main/screens/TitleScreen.gd
@@ -3,9 +3,13 @@ extends "res://main/Screen.gd"
 signal play_local
 signal play_online
 
+onready var coins_label = $Coins/CoinsValueLabel
+onready var anim_player = $Coins/Sprite/AnimationPlayer
+
 func _ready():
 	ShopPurchases.connect("coins_changed", self, "_on_coins_changed")
-	$Coins/CoinsValueLabel.text = str(ShopPurchases.coins)
+	coins_label.text = str(ShopPurchases.coins)
+	anim_player.play("idle")
 
 func _on_LocalButton_pressed() -> void:
 	emit_signal("play_local")
@@ -17,7 +21,16 @@ func _on_CreditsButton_pressed() -> void:
 	ui_layer.show_screen("CreditsScreen")
 
 func _on_coins_changed(new_value) -> void:
-	$Coins/CoinsValueLabel.text = str(new_value)
+	var coins_diff = new_value - int(coins_label.text)
+	
+	if coins_diff > 0:
+		anim_player.play("coin_gain")
+		$Coins/GainLabel.text = "+" + str(coins_diff)
+		yield(anim_player, "animation_finished")
+		anim_player.play("gain_popup")
+	coins_label.text = str(new_value)
+	yield(anim_player, "animation_finished")
+	anim_player.play("idle")
 
 func _on_Shop_pressed():
 	var from: Vector2 
@@ -34,4 +47,6 @@ func _on_Shop_pressed():
 	to, 2.0, 
 	Tween.TRANS_BOUNCE, Tween.EASE_OUT)
 	$Tween.start()
-	
+
+func _on_AddCoinsButton_pressed():
+	ShopPurchases.coins += int(rand_range(76, 145))
